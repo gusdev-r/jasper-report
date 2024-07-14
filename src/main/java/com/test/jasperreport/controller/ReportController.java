ead682404421a6f5861ae43d2c29ff2f9d3112d9
Gustavo Moreira <125270361+gusdev-r@users.noreply.github.com>
2024-07-14 17:26:52 -0300

    refactor: adding service fill parameters function

--- src/main/java/com/test/jasperreport/controller/ReportController.java
+++ src/main/java/com/test/jasperreport/controller/ReportController.java
@@ -1,14 +1,10 @@
 package com.test.jasperreport.controller;
 
 import com.test.jasperreport.domain.Report;
-import com.test.jasperreport.infra.exception.AuthorNotAvailableException;
-import com.test.jasperreport.infra.exception.ContentNotAvailableException;
-import com.test.jasperreport.infra.exception.TitleNotAvailableException;
-import com.test.jasperreport.service.ReportService;
+import com.test.jasperreport.service.FillParametersMainReportTestService;
 import lombok.RequiredArgsConstructor;
 import net.sf.jasperreports.engine.*;
 import net.sf.jasperreports.engine.util.JRLoader;
-import org.apache.coyote.BadRequestException;
 import org.springframework.core.io.ClassPathResource;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
@@ -22,7 +18,6 @@ import org.springframework.web.bind.annotation.RestController;
 import javax.validation.Valid;
 import java.io.IOException;
 import java.io.InputStream;
-import java.util.HashMap;
 import java.util.Map;
 
 @RequiredArgsConstructor
@@ -30,29 +25,18 @@ import java.util.Map;
 @RequestMapping("api/v1/report")
 public class ReportController {
 
-    private final ReportService reportService;
+    private FillParametersMainReportTestService serviceMain;
 
     @PostMapping("/create")
     public ResponseEntity<byte[]> createReport(@RequestBody @Valid Report reportJsonRequest) throws IOException, JRException {
-
-        var validatedAuthor = reportService.validateAuthor(reportJsonRequest.getAuthor());
-        var validatedTitle = reportService.validateTitle(reportJsonRequest.getTitle());
-        var validatedContent = reportService.validateContent(reportJsonRequest.getContent());
-
         InputStream reportStream = new ClassPathResource("reportTemplateTest.jasper").getInputStream();
-
         JasperReport jasperReport = (JasperReport) JRLoader.loadObject(reportStream);
 
-        Map<String, Object> parameters = new HashMap<>();
-        parameters.put("author", validatedAuthor);
-        parameters.put("title", validatedTitle);
-        parameters.put("content", validatedContent);
+        Map<String, Object> parameters = serviceMain.fillReportParam(reportJsonRequest);
 
         JasperPrint jasperPrint = JasperFillManager
                 .fillReport(jasperReport, parameters, new JREmptyDataSource());
-
         byte[] reportBytes = JasperExportManager.exportReportToPdf(jasperPrint);
-
         HttpHeaders headers = new HttpHeaders();
         headers.setContentType(MediaType.APPLICATION_PDF);
         headers.setContentDispositionFormData("inline", "report-ntsec.pdf");
